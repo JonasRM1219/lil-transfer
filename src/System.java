a7e97989d64ce09076b909033591d176ec1a2a07
Lukas <lued1000@stud.ek.dk>
2026-04-16 16:21:56 +0200

    Category ENUM, MediaLibrary, createMedia, CSV-files for movies and series.
    Also short unitTest to make sure data gets parsed correctly

--- src/System.java
+++ src/System.java
@@ -1,2 +1,12 @@
+import java.sql.Array;
+import java.util.ArrayList;
+
 public class System {
+
+    private MediaLibrary mediaLibrary;
+
+    public System(){
+        this.mediaLibrary = new MediaLibrary();
+    }
+
 }
